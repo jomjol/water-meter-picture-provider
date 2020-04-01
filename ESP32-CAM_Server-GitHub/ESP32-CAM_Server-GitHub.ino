33cc102cc4caf77fad90a43025393c920f4b538b
jomjol <30766535+jomjol@users.noreply.github.com>
2020-04-01 22:12:02 +0200

    Update to v2.4.0
    Change in WiFi reconnect handling and blinking evaluation

--- ESP32-CAM_Server-GitHub/ESP32-CAM_Server-GitHub.ino
+++ ESP32-CAM_Server-GitHub/ESP32-CAM_Server-GitHub.ino
@@ -25,7 +25,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Setup start");
   
-  WifiReConnect();
+  WifiInit();
   OTA_setup();
   
   ESP32CAMServer.setup();
@@ -34,6 +34,7 @@ void setup() {
 
 
 void loop() {
+  WifiReConnect();
   ESP32CAMServer.loop();
   OTA_loop();
 }
@@ -41,42 +42,33 @@ void loop() {
 
 void WifiReConnect()
 {
-  pinMode(INT_LED, OUTPUT);         // Initialize the LED_BUILTIN pin as an output
-  digitalWrite(INT_LED, HIGH);      // turn the LED on.
- 
   if (WiFi.status() != WL_CONNECTED)
   {
+      
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.println("");
 
     // Wait for connection
     // attempt to connect to Wifi network:
-    while ( WiFi.status() != WL_CONNECTED) {
+    while ( WiFi.status() != WL_CONNECTED) 
+    {
+      for (int i=0;i<3;i++)
+        {
+        digitalWrite(INT_LED, LOW);          // turn the LED on.
+        delay(200);         // wait
+        digitalWrite(INT_LED, HIGH);         // turn the LED off.
+        delay(200);   // wait
+        delay(0);                        //to prevent watchdog firing.
+        }
       Serial.print("Attempting to connect to WPA network, SSID: ");
       Serial.println(ssid);
       WiFi.begin(ssid, password);
       // wait 10 seconds for connection:
       delay(10000);
     }   
-    
-/*    // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-*/  
-    Serial.println("");
-    Serial.print("Connected to ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    if (MDNS.begin(host)) {
-      Serial.print("MDNS responder started - Host: "); Serial.println(host);
-    }
-
-    for (int i=0;i<3;i++){
+   for (int i=0;i<3;i++)
+    {
       digitalWrite(INT_LED, LOW);          // turn the LED on.
       delay(500);         // wait
       digitalWrite(INT_LED, HIGH);         // turn the LED off.
@@ -84,7 +76,27 @@ void WifiReConnect()
       delay(0);                        //to prevent watchdog firing.
     }
   }
+  
 }
+
+
+void WifiInit()
+{
+  pinMode(INT_LED, OUTPUT);         // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(INT_LED, HIGH);      // turn the LED on.
+
+  WifiReConnect();
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin(host)) {
+      Serial.print("MDNS responder started - Host: "); Serial.println(host);
+  }
+}
+
 
 
 void OTA_setup()
