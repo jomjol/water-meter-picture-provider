edd314efaef2186845fec6c74d3a060bdfa927b5
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2020-06-20 11:35:56 +0200

    Add intensity parameter to internal flashlight

--- ESP32-CAM_Server-GitHub/JomjolGitESP32CAMComm.cpp
+++ ESP32-CAM_Server-GitHub/JomjolGitESP32CAMComm.cpp
@@ -2,12 +2,16 @@
 
 
 // Constructor
-GitESP32CAMCommLibrary::ESP32CAMCommClass::ESP32CAMCommClass(int _pin, int _num_led, int _brightness, int _CS, int _flashPIN, WebServer *_server)
+GitESP32CAMCommLibrary::ESP32CAMCommClass::ESP32CAMCommClass(int _pin, int _num_led, int _brightness, int _CS, int _flashPIN, int _flashChannel, int _flashFreq, int _flashResolution, WebServer *_server)
 {
   light = new Adafruit_NeoPixel(_num_led, _pin, NEO_GRBW + NEO_KHZ800);
   brightness = _brightness;
   flashPIN = _flashPIN;
   NeoPixelPIN = _pin;
+  // for ledc module
+  flashChannel = _flashChannel;
+  flashFreq = _flashFreq;
+  flashResolution = _flashResolution;
 
   pinMode(flashPIN, OUTPUT);         // Initialize the LED_BUILTIN pin as an output
 
@@ -35,7 +39,7 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::camCaptureMITHeader() {
     response += "Content-Type: image/jpeg\r\n";
     response += "Content-Disposition: inline; filename=capture.jpg\r\n\r\n";
     server->sendContent(response);
- 
+
     camera_fb_t * fb = NULL;
     esp_err_t res = ESP_OK;
     int64_t fr_start = esp_timer_get_time();
@@ -56,20 +60,20 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::camCaptureMITHeader() {
       fb_len = fb->len;
       Serial.println("Pixformat = jpeg");
       client.write((const char *)fb->buf, fb->len);
-    } 
+    }
     else {
       Serial.println("Pixformat geht in else");
       jpg_chunking_t jchunk = {client, 0};
       res = frame2jpg_cb(fb, 80, this->jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
       fb_len = jchunk.len;
     }
-	
+
   	esp_camera_fb_return(fb);
 	  int64_t fr_end = esp_timer_get_time();
   	Serial.printf("JPG: %uB %ums\n", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start)/1000));
 }
 
-void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCaptureMITHeader() 
+void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCaptureMITHeader()
 {
     Serial.println(F("CAM Capturing"));
     int total_time = 0;
@@ -89,7 +93,7 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCaptureMITHeader()
 
 void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCaptureWithFlashLight()
 {
-    FlashOn();
+    FlashOn((1 << flashResolution) - 1);
     Serial.println("Flash an");
     delay(5000);
     serverCapture();
@@ -113,13 +117,13 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCapture(uint8_t _quality, 
     if (fr_quality != _quality)
     {
       fr_quality = _quality;
-      s->set_quality(s, fr_quality);    
+      s->set_quality(s, fr_quality);
     }
-  
+
     if (fr_size != _size)
     {
       fr_size = _size;
-      s->set_framesize(s, fr_size);  
+      s->set_framesize(s, fr_size);
     }
     serverCaptureMITHeader();
 }
@@ -127,57 +131,61 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::serverCapture(uint8_t _quality, 
 
 void GitESP32CAMCommLibrary::ESP32CAMCommClass::setup()
 {
-	  camera_config_t config;
-	  config.ledc_channel = LEDC_CHANNEL_0;
-	  config.ledc_timer = LEDC_TIMER_0;
-	  config.pin_d0 = Y2_GPIO_NUM;
-	  config.pin_d1 = Y3_GPIO_NUM;
-	  config.pin_d2 = Y4_GPIO_NUM;
-	  config.pin_d3 = Y5_GPIO_NUM;
-	  config.pin_d4 = Y6_GPIO_NUM;
-	  config.pin_d5 = Y7_GPIO_NUM;
-	  config.pin_d6 = Y8_GPIO_NUM;
-	  config.pin_d7 = Y9_GPIO_NUM;
-	  config.pin_xclk = XCLK_GPIO_NUM;
-	  config.pin_pclk = PCLK_GPIO_NUM;
-	  config.pin_vsync = VSYNC_GPIO_NUM;
-	  config.pin_href = HREF_GPIO_NUM;
-	  config.pin_sscb_sda = SIOD_GPIO_NUM;
-	  config.pin_sscb_scl = SIOC_GPIO_NUM;
-	  config.pin_pwdn = PWDN_GPIO_NUM;
-	  config.pin_reset = RESET_GPIO_NUM;
-	  config.xclk_freq_hz = 20000000;
-	  config.pixel_format = PIXFORMAT_JPEG;
-	  if(psramFound()){
-  		config.frame_size = FRAMESIZE_UXGA;
-  		config.jpeg_quality = 10;
-  		config.fb_count = 2;
-  	  } 
-    else {
-  		config.frame_size = FRAMESIZE_SVGA;
-  		config.jpeg_quality = 12;
-  		config.fb_count = 1;
-	    }
+    Serial.println("LEDC setup");
+    ledcSetup(flashChannel, flashFreq, flashResolution);
+    ledcAttachPin(flashPIN, flashChannel);
 
-  	#if defined(CAMERA_MODEL_ESP_EYE)
-  	  pinMode(13, INPUT_PULLUP);
-  	  pinMode(14, INPUT_PULLUP);
-  	#endif
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    if (psramFound())
+    {
+      config.frame_size = FRAMESIZE_UXGA;
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.jpeg_quality = 12;
+      config.fb_count = 1;
+    }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+    pinMode(13, INPUT_PULLUP);
+    pinMode(14, INPUT_PULLUP);
+#endif
 
 	  // camera init
 	  esp_err_t err = esp_camera_init(&config);
 	  if (err != ESP_OK) {
   		Serial.printf("Camera init failed with error 0x%x", err);
   		return;
-	    }
-  
+	  }
+
 	  sensor_t * s = esp_camera_sensor_get();
 	  //initial sensors are flipped vertically and colors are a bit saturated
 	  if (s->id.PID == OV3660_PID) {
-    		s->set_vflip(s, 1);//flip it back
-    		s->set_brightness(s, 1);//up the blightness just a bit
-    		s->set_saturation(s, -2);//lower the saturation
-    	  }
+      s->set_vflip(s, 1);       //flip it back
+      s->set_brightness(s, 1);  //up the blightness just a bit
+      s->set_saturation(s, -2); //lower the saturation
+    }
 	  //drop down frame size for higher initial frame rate
     fr_size = FRAMESIZE_SVGA;
     s->set_framesize(s, fr_size);
@@ -190,21 +198,26 @@ void GitESP32CAMCommLibrary::ESP32CAMCommClass::setup()
   	  s->set_hmirror(s, 1);
 	  #endif
 
-  light->setBrightness(brightness);
-  light->begin();
-  light->show(); // Initialize all pixels to 'off'
+    light->setBrightness(brightness);
+    light->begin();
+    light->show(); // Initialize all pixels to 'off'
 }
 
-void GitESP32CAMCommLibrary::ESP32CAMCommClass::FlashOn()
+void GitESP32CAMCommLibrary::ESP32CAMCommClass::FlashOn(int intensity)
 {
   Serial.println("FlashOn");
-  digitalWrite(flashPIN, HIGH);      // turn the LED on.
+  int max = (1 << flashResolution) - 1;
+  if (intensity > max)
+  {
+    intensity = max;
+  }
+  ledcWrite(flashChannel, intensity);
 }
 
 void GitESP32CAMCommLibrary::ESP32CAMCommClass::FlashOff()
 {
   Serial.println("FlashOff");
-  digitalWrite(flashPIN, LOW);      // turn the LED on.
+  ledcWrite(flashChannel, LOW);
 }
 
 void GitESP32CAMCommLibrary::ESP32CAMCommClass::LightOn()
